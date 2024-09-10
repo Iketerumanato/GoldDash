849ccc6a3e4390153bdf265ac0b7049a157fb8c5
Nanachi9999 <nnc.dev2021@gmail.com>
2024-09-11 02:28:07 +0900

    各UI配置

---
+++ GoldDashProject/Assets/Resource/Scripts/UDP/PacketClasses.cs
@@ -0,0 +1,317 @@
+using System;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+//パケット系クラスの基底クラス
+public abstract class Packet
+{
+    //バイト配列への変換メソッド実装を強制
+    public abstract byte[] ToByte();
+
+    //バイト配列Aの末尾にバイト配列Bをくっつける
+    protected byte[] AddBytes(byte[] originBytes, byte[] addBytes)
+    {
+        byte[] ret = new byte[originBytes.Length + addBytes.Length];
+
+        for (int i = 0; i < ret.Length; i++)
+        {
+            ret[i] = i < originBytes.Length ? originBytes[i] : addBytes[i - originBytes.Length];
+        }
+
+        return ret;
+    }
+    //バイト配列の末尾に任意のバイトを一つくっつける
+    protected byte[] AddByte(byte[] originBytes, byte addByte)
+    {
+        byte[] ret = new byte[originBytes.Length + 1];
+
+        for (int i = 0; i < ret.Length; i++)
+        {
+            ret[i] = i < originBytes.Length ? originBytes[i] : addByte;
+        }
+
+        return ret;
+    }
+}
+
+//UDPClientから送信するパケットの先頭に付与するカスタムUDPヘッダ。送信番号を持たせて通信交換するとRUDPに進化。おめでとう！
+public class Header : Packet
+{
+    private ushort sessionID; //サーバーから与えるID。セキュリティが要るならハッシュを使うべきか。
+    private ushort indexDiff; //このパケット以降に続くパケット(RUDP用の古いパケット)の位置と、このパケットの先頭インデックスの差を示す
+    private uint sendNum; //このパケットの送信番号
+    private uint ackNum; //最後に相手から受け取ったパケットの送信番号
+    private ushort packetType; //このパケットのタイプ
+    private byte[] data; //データ本体
+
+    //コンストラクタ１　各変数の値を直接指定する
+    public Header(ushort sessionID, ushort indexDiff, uint sendNum, uint ackNum, byte packetType, byte[] data)
+    {
+        this.sessionID = sessionID;
+        this.indexDiff = indexDiff; //古いパケットとの位置関係は送信時に分かるので、引数から直接とればよい
+        this.sendNum = sendNum;
+        this.ackNum = ackNum;
+        this.packetType = packetType;
+        this.data = data;
+    }
+
+    //コンストラクタ２　バイト配列を読んで変数を初期化
+    public Header(byte[] bytes)
+    {
+        int index = 0;
+
+        this.sessionID = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.indexDiff = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.sendNum = BitConverter.ToUInt32(bytes, index);
+        index += sizeof(uint);
+        this.ackNum = BitConverter.ToUInt32(bytes, index);
+        index += sizeof(uint);
+        this.sessionID = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        data = bytes.Skip(index).ToArray();
+    }
+
+    //変数からバイト配列を出力する
+    public override byte[] ToByte()
+    {
+        byte[] ret = new byte[0];
+
+        ret = AddBytes(ret, BitConverter.GetBytes(sessionID));
+        ret = AddBytes(ret, BitConverter.GetBytes(indexDiff));
+        ret = AddBytes(ret, BitConverter.GetBytes(sendNum));
+        ret = AddBytes(ret, BitConverter.GetBytes(ackNum));
+        ret = AddBytes(ret, BitConverter.GetBytes(packetType));
+        ret = AddBytes(ret, data);
+
+        return ret;
+    }
+}
+
+//初回通信用パケット
+public class InitPacketClient : Packet
+{
+    private ushort pass; //マッチング用パスワード
+    private ushort rcvPort; //クライアントが受信用に空けているポートの番号
+    private byte playerNameLength; //プレイヤー名のバイト数
+    private string playerName; //プレイヤー名
+
+    public InitPacketClient(ushort pass, ushort rcvPort, string playerName)
+    {
+        this.pass = pass;
+        this.rcvPort = rcvPort;
+        this.playerName = playerName;
+        this.playerNameLength = (byte)playerName.Length;
+    }
+
+    public InitPacketClient(byte[] bytes)
+    {
+        int index = 0;
+
+        this.pass = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.rcvPort = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.playerNameLength = bytes[index];
+        index++;
+        this.playerName = Encoding.UTF8.GetString(bytes, index, playerNameLength);
+    }
+
+    public override byte[] ToByte()
+    {
+        byte[] ret = new byte[0];
+
+        ret = AddBytes(ret, BitConverter.GetBytes(pass));
+        ret = AddBytes(ret, BitConverter.GetBytes(rcvPort));
+        ret = AddByte(ret, playerNameLength);
+        ret = AddBytes(ret, Encoding.UTF8.GetBytes(playerName));
+
+        return ret;
+    }
+}
+
+public class InitPacketServer : Packet
+{
+    private ushort rcvPort; //サーバのポート番号
+    private ushort sessionID; //サーバーから与えるID
+    private byte state; //現在のサーバの状態を返すプレイヤー名の重複が起きたときなど
+    private byte error; //エラーコード　現在のサーバの状態を返すプレイヤー名の重複が起きたときなど
+
+    public InitPacketServer(ushort rcvPort, ushort sessionID, byte state, byte error)
+    {
+        this.rcvPort = rcvPort;
+        this.sessionID = sessionID;
+        this.state = state;
+        this.error = error;
+    }
+
+    public InitPacketServer(byte[] bytes)
+    {
+        int index = 0;
+
+        this.rcvPort = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.sessionID = BitConverter.ToUInt16(bytes, index);
+        index += sizeof(ushort);
+        this.state = bytes[index];
+        index++;
+        this.error = bytes[index];
+    }
+    public override byte[] ToByte()
+    {
+        byte[] ret = new byte[0];
+
+        ret = AddBytes(ret, BitConverter.GetBytes(rcvPort));
+        ret = AddBytes(ret, BitConverter.GetBytes(sessionID));
+        ret = AddByte(ret, state);
+        ret = AddByte(ret, error);
+
+        return ret;
+    }
+}
+
+public class ActionPacket : Packet
+{
+    byte roughID; //アクションのカテゴリを示す
+    byte detailID; //アクションの詳細な種類を示す
+    byte targetID; //アクションの対象を示す
+    Vector3 pos; //座標データを持つアクションで参照する
+
+    public ActionPacket(byte roughID, byte detailID, byte targetID, UnityEngine.Vector3 pos)
+    {
+        this.roughID = roughID;
+        this.detailID = detailID;
+        this.targetID = targetID;
+        this.pos = pos;
+    }
+
+    public ActionPacket(byte[] bytes)
+    {
+        int index = 0;
+        float x, y, z;
+
+        this.roughID = bytes[index];
+        index++;
+        this.detailID = bytes[index];
+        index++;
+        this.targetID = bytes[index];
+        index++;
+        x = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        y = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        z = BitConverter.ToSingle(bytes, index);
+        pos = new Vector3(x, y, z);
+    }
+
+    public override byte[] ToByte()
+    {
+        byte[] ret = new byte[0];
+
+        ret = AddByte(ret, roughID);
+        ret = AddByte(ret, detailID);
+        ret = AddByte(ret, targetID);
+        ret = AddBytes(ret, BitConverter.GetBytes(pos.x));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos.y));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos.z));
+
+        return ret;
+    }
+}
+
+public class PositionPacket : Packet
+{
+    byte id0;
+    Vector3 pos0;
+    byte id1;
+    Vector3 pos1;
+    byte id2;
+    Vector3 pos2;
+    byte id3;
+    Vector3 pos3;
+
+    public PositionPacket(byte id0, Vector3 pos0, byte id1, Vector3 pos1, byte id2, Vector3 pos2, byte id3, Vector3 pos3)
+    {
+        this.id0 = id0;
+        this.pos0 = pos0;
+        this.id1 = id1;
+        this.pos1 = pos1;
+        this.id2 = id2;
+        this.pos2 = pos2;
+        this.id3 = id3;
+        this.pos3 = pos3;
+    }
+
+    public PositionPacket(byte[] bytes)
+    {
+        int index = 0;
+        float x, y, z;
+
+        //1人目
+        this.id0 = bytes[index];
+        index++;
+        x = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        y = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        z = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        pos0 = new Vector3(x, y, z);
+        //2人目
+        this.id1 = bytes[index];
+        index++;
+        x = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        y = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        z = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        pos1 = new Vector3(x, y, z);
+        //3人目
+        this.id2 = bytes[index];
+        index++;
+        x = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        y = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        z = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        pos2 = new Vector3(x, y, z);
+        //4人目
+        this.id3 = bytes[index];
+        index++;
+        x = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        y = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        z = BitConverter.ToSingle(bytes, index);
+        index += sizeof(float);
+        pos3 = new Vector3(x, y, z);
+    }
+
+    public override byte[] ToByte()
+    {
+        byte[] ret = new byte[0];
+
+        ret = AddByte(ret, id0);
+        ret = AddBytes(ret, BitConverter.GetBytes(pos0.x));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos0.y));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos0.z));
+        ret = AddByte(ret, id1);
+        ret = AddBytes(ret, BitConverter.GetBytes(pos1.x));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos1.y));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos1.z));
+        ret = AddByte(ret, id2);
+        ret = AddBytes(ret, BitConverter.GetBytes(pos2.x));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos2.y));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos2.z));
+        ret = AddByte(ret, id3);
+        ret = AddBytes(ret, BitConverter.GetBytes(pos3.x));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos3.y));
+        ret = AddBytes(ret, BitConverter.GetBytes(pos3.z));
+
+        return ret;
+    }
+}
