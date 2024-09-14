353fd877ef53fe43521f0fd837aa0fe53577ddfe
Nanachi9999 <nnc.dev2021@gmail.com>
2024-09-15 04:47:49 +0900

    テキストが変わる

--- GoldDashProject/Assets/Resource/Scripts/UDP/SubscribeOrderManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/SubscribeOrderManager.cs
@@ -4,6 +4,7 @@ public class SubscribeOrderManager : MonoBehaviour
 {
     [SerializeField] private UdpButtonManager udpButtonManager;
     [SerializeField] private UdpUIDisplayer udpUIDisplayer;
+    [SerializeField] private UdpTextManager udpTextManager;
 
     //subjectの初期化および購読はNull参照頻発地点。ここにエントリーポイントを作って、そこで購買関係の構築順序を制御する
     private void Awake()
@@ -11,5 +12,6 @@ public class SubscribeOrderManager : MonoBehaviour
         udpButtonManager.InitObservation();
 
         udpUIDisplayer.InitObservation(udpButtonManager);
+        udpTextManager.InitObservation(udpButtonManager);
     }
 }
