f0c9941144b5566f326d090e81b29156796c59e6
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-10 18:53:22 +0900

    閾値の初期化と修正

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 /// <summary>
 /// プレイヤー達を管理するクラス。GameServerManagerでSessionIDをKeyとしてDictionaryで管理される
@@ -11,40 +8,32 @@ public class ActorController : MonoBehaviour
     public string PlayerName { set; get; }
     public bool IsRun { set; get; }
     Vector3 oldPos;
-    [SerializeField] float runThreshold;
+    [SerializeField] float runThreshold = 0.1f;
     private float SQR_RunThreshold;
     [SerializeField] Animator PlayerAnimator;
     readonly string RunAnimation = "IsRun";
+    void Start()
+    {
+        SQR_RunThreshold = runThreshold * runThreshold;
+        oldPos = transform.position; // 初期位置を設定
+    }
 
-    //このアクターの座標と向きを更新する
+    // このアクターの座標と向きを更新する
     public void Move(Vector3 pos, Vector3 forward)
     {
+        // 現在の位置と古い位置の差分を計算
+        float distance = (pos - oldPos).sqrMagnitude;
+
+        // 座標が変化していればアニメーションを再生
+        if (distance > SQR_RunThreshold) PlayerAnimator.SetBool(RunAnimation, true);
+        else PlayerAnimator.SetBool(RunAnimation, false);
+
+
+        // 座標と向きを更新
         this.gameObject.transform.position = pos;
         this.gameObject.transform.forward = forward;
-    }
 
-    private void Update()
-    {
-        CheckPlayerMove();
-    }
-
-    void CheckPlayerMove()
-    {
-        // 位置の変化を計算
-        float distance = (transform.position - oldPos).sqrMagnitude;
-
-        // しきい値を超えた場合にアニメーションを再生
-        if (distance > SQR_RunThreshold)
-        {
-            IsRun = true; // 移動中フラグを設定
-            PlayerAnimator.SetBool(RunAnimation, true);
-        }
-        else
-        {
-            IsRun = false; // 停止フラグを設定
-            PlayerAnimator.SetBool(RunAnimation, false);
-        }
-        oldPos = transform.position;
+        oldPos = pos;
     }
 
     //メソッドの例。正式実装ではない
