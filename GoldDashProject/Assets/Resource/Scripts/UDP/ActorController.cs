329afe1f6d3edc4fba35678954bbc25c77d7655e
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-10 19:21:51 +0900

    リファクタ

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -8,26 +8,20 @@ public class ActorController : MonoBehaviour
     public string PlayerName { set; get; }
     public bool IsRun { set; get; }
     Vector3 oldPos;
-    [SerializeField] float runThreshold = 0.1f;
+    [SerializeField] float runThreshold = 0.01f;
     private float SQR_RunThreshold;
     [SerializeField] Animator PlayerAnimator;
     readonly string RunAnimation = "IsRun";
-    void Start()
-    {
-        SQR_RunThreshold = runThreshold * runThreshold;
-        oldPos = transform.position; // 初期位置を設定
-    }
 
     // このアクターの座標と向きを更新する
     public void Move(Vector3 pos, Vector3 forward)
     {
-        // 現在の位置と古い位置の差分を計算
-        float distance = (pos - oldPos).sqrMagnitude;
+        SQR_RunThreshold = runThreshold * runThreshold;
+        oldPos = transform.position; // 初期位置を設定
+        Vector3 distance = pos - oldPos;
 
-        // 座標が変化していればアニメーションを再生
-        if (distance > SQR_RunThreshold) PlayerAnimator.SetBool(RunAnimation, true);
+        if (distance.sqrMagnitude > SQR_RunThreshold) PlayerAnimator.SetBool(RunAnimation, true);
         else PlayerAnimator.SetBool(RunAnimation, false);
-
 
         // 座標と向きを更新
         this.gameObject.transform.position = pos;
