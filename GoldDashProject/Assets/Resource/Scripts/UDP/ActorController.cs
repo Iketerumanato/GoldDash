10313fbc6bb757ad0a600cd62590ecf16615a15c
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-16 02:19:52 +0900

    補間の処理削除

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -8,26 +8,22 @@ public class ActorController : MonoBehaviour
 {
     public string PlayerName { set; get; }
     private Vector3 targetPosition; // サーバーから受信した目標位置
-    private Vector3 predictedPos;   // 予測位置
     private Vector3 oldPos;         // 前回の位置
-    [SerializeField] float positionCorrectionSpeed = 5.0f; // 補正のスピード
     [SerializeField] Animator PlayerAnimator;
     readonly string MoveAnimationStr = "BlendSpeed";
 
     private void Start()
     {
         oldPos = transform.position; // 初期位置を設定
-        predictedPos = transform.position; // 初期の推測位置を現在位置と同じにする
         targetPosition = transform.position; // 初期目標位置
     }
 
     private void Update()
     {
-        // サーバーからの目標位置に向けて滑らかに補間
-        predictedPos = Vector3.Lerp(predictedPos, targetPosition, Time.deltaTime * positionCorrectionSpeed);
-        transform.position = predictedPos;
+        // キャラクターを目標位置に移動
+        transform.position = targetPosition;
 
-        // アニメーションのブレンド
+        // アニメーションのブレンドを更新
         UpdateAnimationBlend();
     }
 
