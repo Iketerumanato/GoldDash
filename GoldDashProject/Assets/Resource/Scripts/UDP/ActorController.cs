1f16e2a2741343301c75008eaac7100182e78719
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-16 00:44:02 +0900

    Update ActorController.cs

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -6,31 +6,34 @@ using UnityEngine;
 public class ActorController : MonoBehaviour
 {
     public string PlayerName { set; get; }
+    public bool IsRun { set; get; }
+    Vector3 oldPos;
     [SerializeField] float runThreshold = 0.01f;
-    [SerializeField] float interpolationSpeed = 5.0f;//動きを補間する速度
     private float SQR_RunThreshold;
     [SerializeField] Animator PlayerAnimator;
-    readonly string MoveAnimationStr = "BlendSpeed";
+    readonly string RunAnimation = "IsRun";
 
     private void Start()
     {
         SQR_RunThreshold = runThreshold * runThreshold;
+        oldPos = transform.position; // 初期位置を設定
     }
 
     // このアクターの座標と向きを更新する
     public void Move(Vector3 pos, Vector3 forward)
     {
-        targetPos = pos;
-        targetRot = Quaternion.LookRotation(forward);
-
-        float distance = (pos - currentPos).sqrMagnitude;
+        float distance = (pos - oldPos).sqrMagnitude;
         float speed = Mathf.Clamp01(distance / SQR_RunThreshold);
-        PlayerAnimator.SetFloat(MoveAnimationStr, speed);
-
-        Debug.Log($"PlayerSpeed : {speed}");
+        PlayerAnimator.SetFloat("BlendSpeed", speed);
 
         //if (distance.sqrMagnitude > SQR_RunThreshold) PlayerAnimator.SetBool(RunAnimation, true);
         //else PlayerAnimator.SetBool(RunAnimation, false);
+
+        // 座標と向きを更新
+        this.gameObject.transform.position = pos;
+        this.gameObject.transform.forward = forward;
+
+        oldPos = pos;
     }
 
     //メソッドの例。正式実装ではない
