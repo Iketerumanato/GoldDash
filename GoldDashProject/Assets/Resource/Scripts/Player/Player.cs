8e850750de893d35821ee983d08da15d9961fb69
Nanachi9999 <nnc.dev2021@gmail.com>
2024-10-17 15:16:15 +0900

    コメントで流れを記述

--- GoldDashProject/Assets/Resource/Scripts/Player/Player.cs
+++ GoldDashProject/Assets/Resource/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public interface IPlayerState
@@ -150,5 +151,74 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Tresure")) drawCircle.enabled = true;
+    }
+
+    //以下手動マージ予定
+    UdpGameClient udpGameClient = null; //パケット送信用。
+
+    //GameClientManagerからプレイヤーの生成タイミングで呼び出してudpGameClientへのアクセスを得る。
+    public void GetUdpGameClient(UdpGameClient udpGameClient)
+    { 
+        this.udpGameClient = udpGameClient;
+    }
+
+    //以下UIの操作などで呼び出されるメソッド。R3でやろっかな
+    //画面を「タッチしたとき」呼ばれる。オブジェクトに触ったかどうか判定
+    private void Interact()
+    {
+        //カメラの位置からレイを飛ばす
+        //RaycastHit hit
+
+        //インタラクト対象でないならreturn。tagで判断　...A
+        //インタラクト対象なら距離を調べる。しきい値より遠いならreturn. ...B
+        //AとB,多分Aの方が処理高速だと思うのでAを先に実行しています
+
+        //プレイヤーならパンチ
+        //Punch(hit.point, hit.distance)
+        //宝箱なら開錠を試みる。消えゆく宝箱だったり、他プレイヤーが使用中の宝箱は開錠できない。
+        //TryOpenChest(hit.point, hit.distance hit.collider.gameObject.GetComponent<ChestController>())
+    }
+
+    //パンチ。パンチを成立させたRaycastHit構造体のPointとDistanceを引数にもらおう
+    private void Punch(Vector3 hitPoint, float distance)
+    { 
+        //distanceを調べてしきい値を調べる
+
+        //射程外なら一人称のスカモーション再生
+        //スカしたことをパケット送信
+
+        //射程内なら一人称のパンチモーション再生
+        //カメラを非同期で敵に向ける処理開始 UniTask
+
+        //パケット送信
+    }
+
+    //宝箱なら開錠を試みる。パンチと同様RaycastHit構造体から引数をもらう。消えゆく宝箱だったり、他プレイヤーが使用中の宝箱は開錠できない。
+    private void TryOpenChest(Vector3 hitPoint, float distance /*ChestController chestController*/)
+    {
+        //既に空いていないたらなにもしない
+
+        //距離が遠すぎる場合、宝箱の付近までオートラン開始。スティック入力があれば即時解除
+        //オートランの目標地点算出
+
+        //宝箱のID取得。サーバー側で開錠中ステータスにする
+        //ACTIONパケット送信
+
+        //キャンバス表示（まわせ）
+
+        //非同期回転処理開始
+        //以下非同期ローカル関数で
+
+        //OpenChest
+
+        //async UniTask OpenChest()
+        //{
+        //    //いつでも中断できるようにする
+
+        //    //成功したらパケット送信準備
+        //    //宝箱のID取得
+
+        //    //パケット送信
+        //}
     }
 }
