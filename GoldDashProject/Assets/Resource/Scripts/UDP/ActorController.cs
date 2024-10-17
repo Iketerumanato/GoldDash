afda95963889124ba8d56f3881ff8ee027ca4d01
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-17 16:27:19 +0900

    補間のロジックをMove内で

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -8,33 +8,34 @@ public class ActorController : MonoBehaviour
 {
     public string PlayerName { set; get; }
     private Vector3 targetPosition;
-    private Vector3 oldPos;         
+    private Vector3 oldPos;
+    private Vector3 currentVelocity;
     [SerializeField] Animator PlayerAnimator;
     [SerializeField] float runThreshold = 0.01f;
     readonly string MoveAnimationStr = "BlendSpeed";
     float SQR_RunThreshold;
 
-    private void Start()
+    private void Awake()
     {
         SQR_RunThreshold = runThreshold * runThreshold;
         oldPos = transform.position;
+        targetPosition = oldPos;
     }
 
-    // このアクターの座標と向きを更新する
     public void Move(Vector3 pos, Vector3 forward)
     {
-        transform.position = Vector3.Lerp(transform.position, pos, 0.1f);
-        transform.forward = Vector3.Lerp(transform.forward, forward, 0.1f);
+        targetPosition = pos;
 
-        float distance = (pos - oldPos).sqrMagnitude;
-        float speed = distance / Time.deltaTime;
-        float smoothSpeed = Mathf.Lerp(PlayerAnimator.GetFloat(MoveAnimationStr), speed, 0.1f);
-        PlayerAnimator.SetFloat(MoveAnimationStr, smoothSpeed);
+        // 補間処理
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, 0.1f); // 0.1fはスムーズさの調整値
 
-        //if (distance.sqrMagnitude > SQR_RunThreshold) PlayerAnimator.SetBool(RunAnimation, true);
-        //else PlayerAnimator.SetBool(RunAnimation, false);
+        float distance = (targetPosition - oldPos).sqrMagnitude;
+        float speed = Mathf.Clamp01(distance / SQR_RunThreshold);
+        PlayerAnimator.SetFloat(MoveAnimationStr, speed);
 
-        oldPos = pos;
+        transform.forward = forward;
+
+        oldPos = targetPosition;
     }
 
     //メソッドの例。正式実装ではない
