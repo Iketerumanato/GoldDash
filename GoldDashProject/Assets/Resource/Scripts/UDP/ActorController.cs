7b8321bc5585ff3fb4b84d4f9d54126572aa5ccc
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-18 17:33:25 +0900

    回転角の大きさでスムージングを行う

--- GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/ActorController.cs
@@ -38,8 +38,10 @@ public class ActorController : MonoBehaviour
         float newSpeed = Mathf.Lerp(currentSpeed, speed, Time.deltaTime * animationLerpSpeed);
         PlayerAnimator.SetFloat(MoveAnimationStr, newSpeed);
 
-        // 向きの更新
-        if (forward != Vector3.zero)
+        float angle = Vector3.Angle(transform.forward, forward);
+
+        // もし回転角が大きければスムーズに回転
+        if (angle > 0.1f) // 0.1f は回転しない閾値
         {
             Quaternion targetRotation = Quaternion.LookRotation(forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
