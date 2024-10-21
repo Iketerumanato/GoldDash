32113222a27a6ae82cad041b801d4c6a0ff098cc
Nanachi9999 <nnc.dev2021@gmail.com>
2024-10-21 18:10:19 +0900

    プロパティであることをやめましょう

--- GoldDashProject/Assets/Resource/Entity.cs
+++ GoldDashProject/Assets/Resource/Entity.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
-    public ushort EntityID { set; get; } //サーバーから割り振られたID。
+    public ushort EntityID; //サーバーから割り振られたID。
     public int Value { set; get; } //整数でデータが必要ならここに格納
 
     public abstract void Init(); //初期化する。変数の初期化など
