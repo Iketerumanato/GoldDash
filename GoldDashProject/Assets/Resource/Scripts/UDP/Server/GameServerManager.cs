8eaa35f8b447cde204a1e75e4f7a69565c23531a
Nanachi9999 <nnc.dev2021@gmail.com>
2024-10-21 17:36:01 +0900

    値が存在するか確認

--- GoldDashProject/Assets/Resource/Scripts/UDP/Server/GameServerManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/Server/GameServerManager.cs
@@ -339,6 +339,8 @@ public class GameServerManager : MonoBehaviour
                                         ushort copiedID = entityID;
 
                                         Debug.Log("ID書き込み");
+                                        Debug.Log(entityDictionary.TryGetValue(entityID, out entity));
+
                                         entityDictionary[entityID].EntityID = copiedID; //値を書き込み
                                         Debug.Log("金額書き込み");
                                         entityDictionary[entityID].Value = lostGold;
