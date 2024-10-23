2bd5143b9c6fec9ca1c487edc337673ee3083e79
Nanachi9999 <nnc.dev2021@gmail.com>
2024-10-24 08:41:36 +0900

    エンティティ仕様の雷を仮作成

---
+++ GoldDashProject/Assets/Resource/ThunderEntity.cs
@@ -0,0 +1,22 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderEntity : Entity
+{
+    public override async void InitEntity() //これが呼ばれてから1000ミリ秒で消える
+    {
+        await UniTask.Delay(1000);
+        DestroyEntity();
+    }
+
+    public override void ActivateEntity()
+    {
+    }
+
+    public override void DestroyEntity()
+    {
+        Destroy(this.gameObject);
+    }
+}
