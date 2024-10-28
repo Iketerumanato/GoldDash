a8c4d49412189fc01f280cc4bbb95fba5854ca95
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-10-28 15:16:00 +0900

    サウンド再生用のスクリプト追加
    内容は空白

---
+++ GoldDashProject/Assets/Resource/Scripts/Sound/SoundPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField] SoundSetting soundSetting;
+    [SerializeField] AudioSource audiosource;
+    
+    public void PlaySoundEffect()
+    {
+
+    }
+}
