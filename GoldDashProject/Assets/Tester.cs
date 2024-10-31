2c35287646e0a98b39485c15545bb70cd33c84f3
Nanachi9999 <nnc.dev2021@gmail.com>
2024-10-31 12:44:47 +0900

    モーション確認用シーンの追加

---
+++ GoldDashProject/Assets/Tester.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, new Vector3(2f, 0.5f, 2f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
