3d5fe9f42182412c287080b2309e6094dbae5446
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-11-04 17:33:04 +0900

    Dotweenの導入とボタンの挙動追加

--- GoldDashProject/Assets/Resource/Scripts/Magic/MagicButton.cs
+++ GoldDashProject/Assets/Resource/Scripts/Magic/MagicButton.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class MagicButton : MonoBehaviour
-{
+public class MagicButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{ 
     [SerializeField] MagicManagement magicManagement;
     [SerializeField] int magicIndex;
     [SerializeField] Button magicbutton;
 
+    [SerializeField] RectTransform buttonRectTransform;
+    private Vector3 originalPosition;
+    [SerializeField] float buttonMoveduration = 20f;
+
     void Start()
     {
+        originalPosition = buttonRectTransform.localPosition;
         magicbutton.onClick.AddListener(() => magicManagement.ActivateMagic(magicIndex));
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // ボタンがHighlightedになったときのアニメーション
+        buttonRectTransform.DOLocalMoveY(originalPosition.y + buttonMoveduration, 0.2f).SetEase(Ease.OutQuad);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // ボタンが元の位置に戻るアニメーション
+        buttonRectTransform.DOLocalMoveY(originalPosition.y, 0.2f).SetEase(Ease.OutQuad);
     }
 }
