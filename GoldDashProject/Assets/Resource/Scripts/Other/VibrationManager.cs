8e124ba695a7395615f0889340c7f77f907245b9
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-11-17 23:00:38 +0900

    タブレットとのAndroidバージョンをBuikdSettiongに合わせる

--- GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
@@ -5,13 +5,35 @@ public class VibrationManager : MonoBehaviour
     public static void VibrateTablet()
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
-        using (AndroidJavaObject vibrationService = new AndroidJavaObject("android.os.Vibrator"))
+        using (AndroidJavaObject unityActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
         {
-            using (AndroidJavaObject unityActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+            using (AndroidJavaObject vibrator = unityActivity.Call<AndroidJavaObject>("getSystemService", "vibrator"))
             {
-                vibrationService.Call("vibrate", 500); // 500msバイブレーション
+                if (vibrator != null)
+                {
+                    // Android API 26 (Oreo)以降でサポートされる場合の振動
+                    if (AndroidVersion() >= 26)
+                    {
+                        AndroidJavaClass vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
+                        AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>("createOneShot", 500, vibrationEffectClass.GetStatic<int>("DEFAULT_AMPLITUDE"));
+                        vibrator.Call("vibrate", vibrationEffect);
+                    }
+                    else
+                    {
+                        // Android API 25以下の振動方法
+                        vibrator.Call("vibrate", 500); // 500ms振動
+                    }
+                }
             }
         }
 #endif
+    }
+
+    private static int AndroidVersion()
+    {
+        using (AndroidJavaClass versionClass = new AndroidJavaClass("android.os.Build$VERSION"))
+        {
+            return versionClass.GetStatic<int>("SDK_INT");
+        }
     }
 }
