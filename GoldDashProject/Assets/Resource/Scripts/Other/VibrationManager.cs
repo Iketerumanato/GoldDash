35055ee5e154441e8d3d751f4713974ee7ec4bb9
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-11-19 18:47:33 +0900

    振動時間と強さを調整可能に

--- GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 
-public class VibrationManager
+public class VibrationManager : MonoBehaviour
 {
+    readonly string VibrationMethod = "vibrate";
+
+    [Tooltip("to 30000")]
+    [SerializeField] long vibrationTime = 250;
+
+    [Tooltip("1 to 255")]
+    [SerializeField,Range(1,255)] int vibratePower = 75;
+
     public void VibrateWithAmplitude(long milliseconds, int amplitude)
     {
 #if UNITY_ANDROID
@@ -19,26 +27,20 @@ public class VibrationManager
             AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>(
                 "createOneShot",
                 milliseconds,
-                amplitude
-            );
+                amplitude);
             // 振動処理を呼び出す
-            vibrator.Call("vibrate", vibrationEffect);
+            vibrator.Call(VibrationMethod, vibrationEffect);
         }
 #endif
     }
 
-    /// <summary>
-    /// 現在のAndroidバージョンが8.0（API 26）以上かどうかを確認します。
-    /// </summary>
-    /// <returns>API 26以上ならtrue、それ以外はfalse</returns>
+    //現在のAndroidバージョンが8.0（API 26）以上かどうかを確認
     private bool AndroidVersionIsOreoOrHigher()
     {
 #if UNITY_ANDROID
         AndroidJavaClass versionClass = new AndroidJavaClass("android.os.Build$VERSION");
         int sdkInt = versionClass.GetStatic<int>("SDK_INT");
-        return sdkInt >= 26; // API 26 = Android 8.0
-#else
-        return false;
+        return sdkInt >= 26;
 #endif
     }
 }
