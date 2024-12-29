12e0580572dbe3ae115b87a6ce7185712e9914e7
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-12-29 16:20:34 +0900

    ゲーム開始時キャンバスを非表示

--- GoldDashProject/Assets/Resource/Scripts/UDP/UI/TitleUI.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/UI/TitleUI.cs
@@ -24,10 +24,10 @@ public class TitleUI : MonoBehaviour
     #region クライアントモードのオブジェクト群
     [Header("Mode_Logoのオブジェクトたち")]
     [SerializeField] GameObject StartGameButton;
-    [SerializeField] TMP_Text GameTitle;
+    [SerializeField] GameObject GameTitle;
 
     [Header("Mode_Settingのオブジェクトたち")]
-    [SerializeField] TMP_InputField PlayerNameSetting;
+    [SerializeField] GameObject PlayerNameSetting;
     [SerializeField] GameObject StartConnectButton;
 
     [Header("前に戻るボタン")]
@@ -41,20 +41,22 @@ public class TitleUI : MonoBehaviour
     //[Header("サーバーの画面で扱われるオブジェクト(主に演出の)")]
     #endregion
 
+    [SerializeField] GameObject[] TitleUICamvas;
+
     private void Start()
     {
         _title.InitObservationClient(_title);
-        InitObserver(_title);
+        InitObserver(_title,_gameClientManager,_gameServerManager);
         _gameClientManager.InitObservation(_title);
         _gameServerManager.InitObservation(_title);
         _mapGenerator.InitObservation(_gameServerManager, _gameClientManager);
     }
 
-    public void InitObserver(Title title)//,GameClientManager gameClientManager,GameServerManager gameServerManager)
+    public void InitObserver(Title title,GameClientManager gameClientManager,GameServerManager gameServerManager)
     {
         title.titleButtonSubject.Subscribe(buttonevent => ProcessUdpManagerEvent(buttonevent));
-        //gameServerManager.ServerInternalSubject.Subscribe(e => ProcessServerInternalEvent(e));
-        //gameClientManager.ClientInternalSubject.Subscribe(e => ProcessClientInternalEvent(e));
+        gameServerManager.ServerInternalSubject.Subscribe(e => ProcessServerInternalEvent(e));
+        gameClientManager.ClientInternalSubject.Subscribe(e => ProcessClientInternalEvent(e));
     }
 
     private void ProcessUdpManagerEvent(Title.TITLE_BUTTON_EVENT clientbuttonEvent)
@@ -65,22 +67,25 @@ public class TitleUI : MonoBehaviour
                 //非表示
                 StartClientButton.SetActive(false);
                 StartServerButton.SetActive(false);
+
                 //表示
                 StartGameButton.SetActive(true);
-                GameTitle.enabled = true;
+                GameTitle.SetActive(true);
                 _title.ChangeStateClient(Title.CLIENT_MODE.MODE_LOGO);
                 break;
+
             case Title.TITLE_BUTTON_EVENT.BUTTON_CLIENT_GO_SETTING:
                 StartGameButton.SetActive(false);
-                GameTitle.enabled = false;
+                GameTitle.SetActive(false);
 
-                PlayerNameSetting.enabled = true;
+                PlayerNameSetting.SetActive(true);
                 StartConnectButton.SetActive(true);
                 TitleExplanationText[0].text = "TAP_TO_CONNECT";
                 _title.ChangeStateClient(Title.CLIENT_MODE.MODE_SETTING);
                 break;
+
             case Title.TITLE_BUTTON_EVENT.BUTTON_CLIENT_CONNECT:
-                PlayerNameSetting.enabled = false;
+                PlayerNameSetting.SetActive(false);
                 StartConnectButton.SetActive(false);
                 _title.ChangeStateClient(Title.CLIENT_MODE.MODE_WAITING);
                 break;
@@ -90,6 +95,43 @@ public class TitleUI : MonoBehaviour
                 StartServerButton.SetActive(false);
                 TitleExplanationText[0].text = "WAITING_CONNECT";
                 _title.ChangeStateServer(Title.SERVER_MODE.MODE_ACTIVATE);
+                break;
+        }
+    }
+
+    private void ProcessServerInternalEvent(GameServerManager.SERVER_INTERNAL_EVENT e)
+    {
+        switch (e)
+        {
+            case GameServerManager.SERVER_INTERNAL_EVENT.EDIT_GUI_FOR_GAME:
+                //非表示
+                TitleUICamvas[0].SetActive(false);
+                TitleUICamvas[1].SetActive(false);
+                //表示
+                //originSignMini.gameObject.SetActive(true);
+                //serverSignMini.gameObject.SetActive(true);
+                //stateMessageMini.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ProcessClientInternalEvent(GameClientManager.CLIENT_INTERNAL_EVENT e)
+    {
+        switch (e)
+        {
+            case GameClientManager.CLIENT_INTERNAL_EVENT.EDIT_GUI_FOR_GAME:
+                //非表示
+                TitleUICamvas[0].SetActive(false);
+                TitleUICamvas[1].SetActive(false);
+
+                //表示
+                //originSignMini.gameObject.SetActive(true);
+                //clientSignMini.gameObject.SetActive(true);
+                //stateMessageMini.gameObject.SetActive(true);
+                break;
+            default:
                 break;
         }
     }
