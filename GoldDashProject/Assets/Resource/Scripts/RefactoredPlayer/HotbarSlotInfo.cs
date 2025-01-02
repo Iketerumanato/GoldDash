400f75f0db059661a6eb5ba677cb393cf9e5c483
Nanachi9999 <nnc.dev2021@gmail.com>
2025-01-02 15:05:20 +0900

    ホットバー　理論上動く

---
+++ GoldDashProject/Assets/Resource/Scripts/RefactoredPlayer/HotbarSlotInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotbarSlotInfo : MonoBehaviour
+{
+    [SerializeField] public int slotNum;
+    public Definer.MID magicID;
+}
--- GoldDashProject/Assets/Resource/Scripts/RefactoredPlayer/UIDisplayer.cs
+++ GoldDashProject/Assets/Resource/Scripts/RefactoredPlayer/UIDisplayer.cs
@@ -6,33 +6,35 @@ using UnityEngine;
 public class UIDisplayer : MonoBehaviour
 {
     //各種巻物のUI
-    [SerializeField] private List<MIDandUIpair> m_magicIDandUIobjectPair;
+    [Header("magicIDと、それに対応する説明文オブジェクトの参照")]
+    [SerializeField] private List<MIDAndExplainObjects> m_magicIDAndExplainPrefabList;
 
     //KeyValuePairはインスペクタで表示できない（Serializableでない）ので自前のペアを作って使う
     [Serializable]
-    private class MIDandUIpair
+    private class MIDAndExplainObjects
     {
         public Definer.MID magicID;
-        public GameObject scrollUISet;
+        public GameObject scrollExplainPrefab;
     }
 
     //高速に検索するためのDictionary
-    private Dictionary<Definer.MID, GameObject> m_magicIDandUIobjectDictionary;
+    private Dictionary<Definer.MID, MIDAndExplainObjects> m_magicIDAndExplainPrefabDictionary;
 
     //バーチャルスティック
+    [Header("スティックUIのゲームオブジェクト")]
     [SerializeField] private GameObject m_variableJoystick;
     [SerializeField] private GameObject m_dynamicJoystick;
 
-    //メッセージUI
-    [SerializeField] private GameObject m_messageUI;
+    [Header("ホットバー全体の親オブジェクト")]
+    [SerializeField] private GameObject m_HotbarParent;
 
     private void Start()
     {
         //インスペクタで設定した値をDictionaryに登録する
-        m_magicIDandUIobjectDictionary = new Dictionary<Definer.MID, GameObject>();
-        foreach (MIDandUIpair m in m_magicIDandUIobjectPair)
+        m_magicIDAndExplainPrefabDictionary = new Dictionary<Definer.MID, MIDAndExplainObjects>();
+        foreach (MIDAndExplainObjects m in m_magicIDAndExplainPrefabList)
         {
-            m_magicIDandUIobjectDictionary.Add(m.magicID, m.scrollUISet);
+            m_magicIDAndExplainPrefabDictionary.Add(m.magicID, m);
         }
     }
 
@@ -45,24 +47,27 @@ public class UIDisplayer : MonoBehaviour
     {
         switch (state)
         {
-            case PLAYER_STATE.OPENING_CHEST: //宝箱の開錠中、移動スティックを非表示にする
-                m_variableJoystick.SetActive(false);
+            case PLAYER_STATE.OPENING_CHEST:
+                m_variableJoystick.SetActive(false); //宝箱の開錠中、移動スティック非表示
                 m_dynamicJoystick.SetActive(false);
+                m_HotbarParent.SetActive(false); //ホットバー非表示
                 break;
             case PLAYER_STATE.USING_SCROLL: //巻物を開いたとき、巻物に表示するUIを決定する
-                foreach (KeyValuePair<Definer.MID, GameObject> k in m_magicIDandUIobjectDictionary) //表示中の巻物UIをすべて非表示にする
-                { 
-                    k.Value.SetActive(false);
-                }
-                GameObject currentScrollUISet; //null参照防止にTryGetValueしておく
-                if (m_magicIDandUIobjectDictionary.TryGetValue(magicID, out currentScrollUISet)) //取得に成功したら
+                foreach (KeyValuePair<Definer.MID, MIDAndExplainObjects> k in m_magicIDAndExplainPrefabDictionary) //表示中の巻物UIをすべて非表示にする
                 {
-                    currentScrollUISet.SetActive(true); //magicIDに対応した巻物UIを表示する
+                    k.Value.scrollExplainPrefab.SetActive(false);
                 }
+                MIDAndExplainObjects currentScrollUISet; //null参照防止にTryGetValueしておく
+                if (m_magicIDAndExplainPrefabDictionary.TryGetValue(magicID, out currentScrollUISet)) //取得に成功したら
+                {
+                    currentScrollUISet.scrollExplainPrefab.SetActive(true); //magicIDに対応した巻物UIを表示する
+                }
+                m_HotbarParent.SetActive(false); //ホットバー非表示
                 break;
             default: //基本的にすべて表示する
                 m_variableJoystick.SetActive(true);
                 m_dynamicJoystick.SetActive(true);
+                m_HotbarParent.SetActive(true);
                 break;
         }
     }
