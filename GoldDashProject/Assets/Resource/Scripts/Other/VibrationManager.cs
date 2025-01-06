c5d2f4ee2cfbb110be919ae03ce59675a962b08a
Nanachi9999 <nnc.dev2021@gmail.com>
2025-01-06 18:54:22 +0900

    旧Player削除

--- GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/Other/VibrationManager.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 
 public class VibrationManager : MonoBehaviour
 {
-    readonly string VibrationMethod = "vibrate";
+//    readonly string VibrationMethod = "vibrate";
 
-    //millisecondsに関しては30000ミリ秒,amplitudeに関しては1から255まで強さを設定できる
-    public void VibrateWithAmplitude(long vibrationTime, int vibratePower)
-    {
-#if UNITY_ANDROID
-        // UnityPlayer クラスの取得
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-        // Vibratorサービスを取得
-        AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+//    //millisecondsに関しては30000ミリ秒,amplitudeに関しては1から255まで強さを設定できる
+//    public void VibrateWithAmplitude(long vibrationTime, int vibratePower)
+//    {
+//#if UNITY_ANDROID
+//        // UnityPlayer クラスの取得
+//        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+//        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+//        // Vibratorサービスを取得
+//        AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
 
-        //AndroidVersionIsOreoOrHigherでtrue判定を受けた時のみVibrationEffectを利用
-        if (vibrator != null && AndroidVersionIsOreoOrHigher())
-        {
-            // VibrationEffectクラスの生成
-            AndroidJavaClass vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
-            AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>(
-                "createOneShot",
-                vibrationTime,
-                vibratePower);
-            // 振動処理を呼び出す
-            vibrator.Call(VibrationMethod, vibrationEffect);
-        }
-#endif
-    }
+//        //AndroidVersionIsOreoOrHigherでtrue判定を受けた時のみVibrationEffectを利用
+//        if (vibrator != null && AndroidVersionIsOreoOrHigher())
+//        {
+//            // VibrationEffectクラスの生成
+//            AndroidJavaClass vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
+//            AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>(
+//                "createOneShot",
+//                vibrationTime,
+//                vibratePower);
+//            // 振動処理を呼び出す
+//            vibrator.Call(VibrationMethod, vibrationEffect);
+//        }
+//#endif
+//    }
 
 //    //現在のAndroidバージョンが8.0（API 26）以上かどうかを確認
 //    private bool AndroidVersionIsOreoOrHigher()
