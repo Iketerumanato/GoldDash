23d3d4549f1b1f99a2f8950d6b54a363cf7edb32
teru <74332407+Iketerumanato@users.noreply.github.com>
2025-01-12 17:59:40 +0900

    Update CenterLogoImageAnimation.cs

--- GoldDashProject/Assets/CenterLogoImageAnimation.cs
+++ GoldDashProject/Assets/CenterLogoImageAnimation.cs
@@ -18,9 +18,9 @@ public class CenterLogoImageAnimation : MonoBehaviour
 
     private void CenterLogoAnimationMain()
     {
-        CenterLogoAnimation.Append(CenterLogoImageTransform.DOLocalRotate(new Vector3(0f, 0f, 360f), startRotateSpeed, RotateMode.FastBeyond360).SetEase(Ease.InOutBack))
-            .SetDelay(animationDylayTime)
-            .Append(CenterLogoImageTransform.DOLocalRotate(new Vector3(0f, 0f, 360f), secondRotateSpeed, RotateMode.FastBeyond360).SetEase(Ease.OutBack))
-            .SetLoops(-1);
+        CenterLogoAnimation.Append(CenterLogoImageTransform.DOLocalRotate(new Vector3(0f, 0f, 360f), startRotateSpeed, RotateMode.FastBeyond360).SetEase(Ease.InOutBack))//InOutBackを付けつつ一回目の回転
+            .SetDelay(animationDylayTime)//少し待機
+            .Append(CenterLogoImageTransform.DOLocalRotate(new Vector3(0f, 0f, 360f), secondRotateSpeed, RotateMode.FastBeyond360).SetEase(Ease.OutBack))//InOutBackでの回転速度に追いつくためOutBackで２回目の回転
+            .SetLoops(-1);//無限ループ
     }
 }
