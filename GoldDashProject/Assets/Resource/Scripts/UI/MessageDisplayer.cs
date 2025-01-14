39d67e53e0273e61f06c5b805f4c937a358954e9
Nanachi9999 <nnc.dev2021@gmail.com>
2025-01-14 15:05:41 +0900

    プレイヤープレハブから余計なもの消去

--- GoldDashProject/Assets/Resource/Scripts/UI/MessageDisplayer.cs
+++ GoldDashProject/Assets/Resource/Scripts/UI/MessageDisplayer.cs
@@ -23,12 +23,6 @@ public class MessageDisplayer : MonoBehaviour
     [Range(0f, 1f)]
     [SerializeField] float largeTextFadeDuration = 0.5f;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.X)) { DisplaySmallMessage($"{TestGoldNum} Gを手に入れた!"); }
-        if (Input.GetKeyDown(KeyCode.Z)) { DisplayLargeMessage($"あと 1分 !", lageTextDisplayTime); }
-    }
-
     #region SmallMessageの処理
     // 画面右のメッセージの表示
     public void DisplaySmallMessage(string text)
