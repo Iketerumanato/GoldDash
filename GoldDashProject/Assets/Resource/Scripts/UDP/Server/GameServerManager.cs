bc0cb84cc2f91a7dd6f2e5a40f767cc6419dc778
Nanachi9999 <nnc.dev2021@gmail.com>
2025-01-15 19:08:09 +0900

    アプリケーション再起動作戦

--- GoldDashProject/Assets/Resource/Scripts/UDP/Server/GameServerManager.cs
+++ GoldDashProject/Assets/Resource/Scripts/UDP/Server/GameServerManager.cs
@@ -395,8 +395,12 @@ public class GameServerManager : MonoBehaviour
             {
                 gameServerManager.blackImage.DOFade(1f, 0.3f).OnComplete(() =>
                 {
-                    DOTween.Clear(true);
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+#if UNITY_STANDALONE_WIN
+                System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+                Application.Quit();
+#else
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
                 });
             });
         }
