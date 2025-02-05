57feb48eb758d7d2d33727fe655473130d07385a
Nanachi9999 <nnc.dev2021@gmail.com>
2025-02-05 15:19:01 +0900

    カメラの縦角度制限を上下個別に設定する

--- GoldDashProject/Assets/Resource/Scripts/RefactoredPlayer/PlayerCameraController.cs
+++ GoldDashProject/Assets/Resource/Scripts/RefactoredPlayer/PlayerCameraController.cs
@@ -16,9 +16,13 @@ public class PlayerCameraController : MonoBehaviour
     [Header("カメラ回転速度（度／毎秒）")]
     [SerializeField] private float m_cameraMoveSpeed = 1f;
 
-    [Header("カメラの縦方向（X軸中心）回転の角度制限")]
+    [Header("カメラの縦方向（X軸中心）回転の上向き角度制限")]
     [Range(0f, 90f)]
-    [SerializeField] private float m_camRotateLimitX = 90f;
+    [SerializeField] private float m_camRotateLimitX_Upper = 90f;
+
+    [Header("カメラの縦方向（X軸中心）回転の下向き角度制限")]
+    [Range(0f, 90f)]
+    [SerializeField] private float m_camRotateLimitX_Lower = 90f;
 
     //現在のカメラのオイラー角をxのみ保存
     private float m_rotationX;
@@ -43,7 +47,7 @@ public class PlayerCameraController : MonoBehaviour
             //前提：カメラはZ軸の負の方向を向いている
             //垂直の入力を使って、カメラのみ、X軸中心回転を行う。
             m_rotationX -= D_InputVertcal * m_cameraMoveSpeed * Time.deltaTime; //Unityは左手座標系なので、上下の回転角度（X軸中心）にはマイナスをかけなければならない
-            m_rotationX = Mathf.Clamp(m_rotationX, -m_camRotateLimitX, m_camRotateLimitX); //縦方向(X軸中心)回転には角度制限をつけないと宙返りしてしまう
+            m_rotationX = Mathf.Clamp(m_rotationX, -m_camRotateLimitX_Upper, m_camRotateLimitX_Lower); //縦方向(X軸中心)回転には角度制限をつけないと宙返りしてしまう
             m_playerCamera.transform.eulerAngles = new Vector3(m_rotationX, m_playerCamera.transform.eulerAngles.y, 0f); //m_playerCamera.transform.eulerAngles.yは親の回転に委ねているので弄らない
         }
     }
